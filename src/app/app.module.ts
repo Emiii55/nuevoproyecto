b55f9c2df1ed61e4345cc745c42cf52e58b5d890
Emiii55 <emifransil@gmail.com>
2024-04-10 11:04:31 -0300

    agregue navbar

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { GaleriaComponent } from './principal/galeria/galeria.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalComponent,
+    GaleriaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
