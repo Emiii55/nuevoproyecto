72eb84c08131080312aaab0b2969e090b70e37f1
zanahoria6 <lopezruiztomas59@gmail.com>
2024-04-30 09:34:36 -0300

    configurar interfaz

--- src/app/models/animal.ts
+++ src/app/models/animal.ts
@@ -2,4 +2,6 @@ export interface Animal {
     especie: string;
     edad: number;
     genero: string;
+    imagen: string;
+    alt: string;
 }
--- src/app/modules/inicio/pages/inicio/inicio.component.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//importamos interfaz
+import { Animal } from 'src/app/models/animal';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  //propiedad publica (tipo array)
+  public info: Animal[];
 
+  constructor() {
+    this.info = [
+      {
+        especie: "tonto",
+        edad: 0,
+        genero: "macho",
+        imagen: "https://www.eltesorodelajumentud.info/labradrs.jpg",
+        alt: "imagen de perrito"
+      }
+    ]
+  }
 }
