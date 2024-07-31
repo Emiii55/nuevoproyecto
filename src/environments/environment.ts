e905b5a573cd38a989f654674a902e348b18c0c6
Emiii55 <emifransil@gmail.com>
2024-07-31 10:44:17 -0300

    cree el modulo para productos, modifique el footer, navbar y carrusel

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// CONFIGURACIÓN DE FIREBASE EN NUESTRA APLICACIÓN WEB
+export const environment = {
+    production: false,
+
+    // sacamos la palabra "const" y el ";" final, cambiamos el "=" por ":"
+    firebaseConfig : {
+        apiKey: "AIzaSyAX4xCMrBTrOPq1mir08-kGSmJm26Re_ps",
+        authDomain: "tpprogramacion-2024.firebaseapp.com",
+        projectId: "tpprogramacion-2024",
+        storageBucket: "tpprogramacion-2024.appspot.com",
+        messagingSenderId: "347920148007",
+        appId: "1:347920148007:web:f574724ea29ec606c6d002"
+    }
+}
+
+
+
+
+
